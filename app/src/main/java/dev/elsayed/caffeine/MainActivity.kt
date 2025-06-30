b4901822ba7dd0f2e9537781d6264c43fd5cef1d
Elsayed Magdy <saikoo1239@gmail.com>
2025-06-30 22:53:21 +0300

    Initial commit

---
+++ app/src/main/java/dev/elsayed/caffeine/MainActivity.kt
@@ -0,0 +1,26 @@
+package dev.elsayed.caffeine
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.elsayed.caffeine.ui.theme.CaffeineTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CaffeineTheme {
+
+            }
+        }
+    }
+}
