4c95aac33c29ba4508abd87c27268545d0742ea1
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-08 19:36:58 +0300

    refactor : moved app module to new file

--- app/src/main/java/dev/elsayed/caffeine/CaffeineApplication.kt
+++ app/src/main/java/dev/elsayed/caffeine/CaffeineApplication.kt
@@ -1,16 +1,16 @@
 package dev.elsayed.caffeine
 
 import android.app.Application
+import dev.elsayed.caffeine.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
 
 class CaffeineApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-//        startKoin {
-//            androidContext(this@CaffeineApplication)
-//            modules(appModule)
-//        }
+        startKoin {
+            androidContext(this@CaffeineApplication)
+            modules(appModule)
+       }
     }
 }
-//val appModule = module {
-//    viewModelOf(::CustomizationCoffeeViewModel)
-//}
