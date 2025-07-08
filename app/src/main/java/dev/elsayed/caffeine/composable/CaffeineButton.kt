bcbebbc86a1fe13d9aee3845098cbf2f987504a1
Elsayed Magdy <saikoo1239@gmail.com>
2025-07-08 19:38:52 +0300

    refactor : enhance button implementation

--- app/src/main/java/dev/elsayed/caffeine/composable/CaffeineButton.kt
+++ app/src/main/java/dev/elsayed/caffeine/composable/CaffeineButton.kt
@@ -1,23 +1,15 @@
 package dev.elsayed.caffeine.composable
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -31,23 +23,22 @@ import dev.elsayed.caffeine.ui.theme.Urbanist
 fun CaffeineButton(
     text:String ,
     icon: ImageVector,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onNavClick : ()-> Unit = {}
 ) {
-    Row(
+    Button(
+        onClick = onNavClick,
         modifier = modifier
             .padding(bottom = 50.dp)
             .height(56.dp)
-            .shadow(8.dp, RoundedCornerShape(100.dp))
-            .clip(RoundedCornerShape(100.dp))
-            .background(Theme.colors.black)
-            .padding(horizontal = 32.dp, vertical = 0.dp),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+            .shadow(8.dp, RoundedCornerShape(100.dp)),
+        shape = RoundedCornerShape(100.dp),
+        contentPadding = PaddingValues(horizontal = 32.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Theme.colors.black)
     ) {
         Text(
             color = Theme.colors.white87,
             text = text,
-            modifier = Modifier,
             fontSize = 16.sp,
             fontWeight = FontWeight.Bold,
             fontFamily = Urbanist
